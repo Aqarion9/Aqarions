4ca633712804f8b2ef2c3aabb6f7026fded08ea7
James Aaron <crawleylavey@gmail.com>
2025-12-10 23:32:00 -0500

    Create Serializers.py

---
+++ mkdir aqarionz-node && cd aqarionz-node python -m venv venv source venv/bin/activate  # (Windows: venv\Scripts\activate) pip install django djangorestframework django-admin startproject aqarionz . django-admin startapp graph/Graph/Serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Node, Link
+
+class NodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Node
+        fields = '__all__'
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = '__all__'
